ea88ee1d7145e58c676e1b013391a0000d7a4bfa
Raisul Amin Hasib <raisulaminhasib@gmail.com>
2021-08-25 02:26:06 +0800

    Adding Google Auth

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require ('mongoose-findorcreate');
+const HttpsProxyAgent = require('https-proxy-agent');
+
 
 const app = express();
 
@@ -44,22 +48,91 @@ mongoose.set("useCreateIndex", true);
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
+  googleId:String
 });
 
+
+
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+
+
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+
+//Google Auth this section first part is old
+
+
+// passport.use(new GoogleStrategy({
+//     clientID: process.env.CLIENT_ID,
+//     clientSecret: process.env.CLIENT_SECRET,
+//     callbackURL: "http://localhost:3000/auth/google/secrets",
+//     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+//   },
+//   function(accessToken, refreshToken, profile, cb) {
+//     User.findOrCreate({ googleId: profile.id }, function (err, user) {
+//         console.log(profile);
+//       return cb(err, user);
+//     });
+//   }
+// ));
+
+///new google auth with setting up the proxy.
+
+const gStrategy = new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    return cb(err, user);
+    });
+    }
+    );
+    const agent = new HttpsProxyAgent(process.env.HTTP_PROXY || "http://127.0.0.1:7890");
+    gStrategy._oauth2.setAgent(agent);
+
+    passport.use(gStrategy);
+
+
+
+
 
 ///end///
 
 app.get("/", function (req, res) {
   res.render("home");
 });
+
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] })
+  );
+
+  app.get("/auth/google/secrets",
+  passport.authenticate('google', { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+  });
+
+
 
 app.get("/login", function (req, res) {
   res.render("login");
